5ef9970fb6a236857b3e543a7b81382052f48409
davidthurm <iosgeek@hotmail.com>
2023-10-01 19:16:12 -0500

    Create providers.tf

---
+++ moderate_terraform/variables/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+  required_version = "~> 1.5.7"
+}
+
+provider "aws" {
+  # Configuration options
+}
