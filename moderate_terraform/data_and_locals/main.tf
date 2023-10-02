509d21dafa9fec6900cf0ea57c3ec4d9e7b0fc78
davidthurm <iosgeek@hotmail.com>
2023-10-01 19:30:03 -0500

    Update main.tf

--- moderate_terraform/data_and_locals/main.tf
+++ moderate_terraform/data_and_locals/main.tf
@@ -1,1 +1,23 @@
+locals {
+  name              = "dev-amazon2023"
+  service_name      = "example"
+  owner             = "utahsaint"
+  environment       = "dev"
+  terraform_code    = "advanced_terraform_v2"
+}
+locals {
+  # Common tags to be assigned to all resources
+  common_tags = {
+    Name        = local.name
+    Service     = local.service_name
+    Owner       = local.owner
+    Environment = local.environment
+    Terraform   = local.terraform_code
+  }
+}
 
+
+resource "aws_instance" "example" {
+  ami  = data.aws_ami.latest_amazon_linux_2.id 
+  tags = local.common_tags
+}
