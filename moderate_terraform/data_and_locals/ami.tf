d42d9cafb59644d1babbfac434ca6e97120c5345
davidthurm <iosgeek@hotmail.com>
2023-10-01 19:22:11 -0500

    Create ami.tf

---
+++ moderate_terraform/data_and_locals/ami.tf
@@ -0,0 +1,65 @@
+###########################################################
+## /saintcon23/moderate_terraform/data_and_locals/ami.tf ##
+###########################################################
+data "aws_ami" "latest_amazon_linux_2" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["al2023-ami-minimal-2023*"]
+  }
+  filter {
+    name = "owner-id"
+    values = ["137112412989"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }  
+  filter {
+    name = "owner-alias"
+    values = ["amazon"]  
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "hypervisor"
+    values = ["xen"]
+  }
+  filter {
+    name   = "image-type"
+    values = ["machine"]
+  }
+  filter {
+    name   = "ena-support"
+    values = ["true"]
+  }
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp3"]
+  }
+  filter {
+    name   = "block-device-mapping.delete-on-termination"
+    values = ["true"]
+  }
+  filter {
+    name   = "block-device-mapping.device-name"
+    values = ["/dev/xvda"]
+  }
+  filter {
+    name   = "block-device-mapping.encrypted"
+    values = ["false"]
+  }
+}
+output "latest_amazon_linux_2_ami_id" {
+  value = data.aws_ami.latest_amazon_linux_2.id
+}
